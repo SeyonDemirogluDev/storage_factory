7d702c3048969bc98266585b3e5785fa144164fc
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-12 21:37:32 +0200

    Added SimpleStorage file

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,27 +4,27 @@ pragma solidity ^0.8.0;
 
 contract SimpleStorage {
 
-    uint256 favNumber;
+    uint256 favoriteNumber;
 
     struct Person {
         string name;
-        uint256 favNumber;
+        uint256 favoriteNumber;
     }
 
     Person[] public getPersonAndNumber;
 
     mapping(string => uint256) public getPersonNumber;
 
-    function setPerson(string memory _name, uint256 _favNumber) public {
-        getPersonAndNumber.push(Person(_name, _favNumber));
-        getPersonNumber[_name] = _favNumber;
+    function setPerson(string memory _name, uint256 _favoriteNumber) public {
+        getPersonAndNumber.push(Person(_name, _favoriteNumber));
+        getPersonNumber[_name] = _favoriteNumber;
     }
 
-    function setFavoriteNumber(uint256 _favNumber) public {
-        favNumber = _favNumber;
+    function setFavoriteNumber(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
     }
 
     function getFavoriteNumber() public view returns (uint256) {
-        return favNumber;
+        return favoriteNumber;
     }
 }
