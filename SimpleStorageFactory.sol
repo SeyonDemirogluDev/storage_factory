38f8d61fdc4d7fd843d26d5f653d74401c59c761
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-17 21:54:29 +0200

    Add SimpleStorageFactory

--- SimpleStorageFactory.sol
+++ SimpleStorageFactory.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Unlicense
 
 pragma solidity ^0.8.0;
 
@@ -8,16 +8,16 @@ contract SimpleStorageFactory {
 
     SimpleStorage[] public getSimpleStorageContract;
     
-    function setSimpleStorageContract() public {
+    function setSimpleStorageContract() external {
         SimpleStorage simpleStorage = new SimpleStorage();
         getSimpleStorageContract.push(simpleStorage);
     }
     
-    function setStorageFactoryNumber(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+    function setStorageFactoryNumber(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) external {
         getSimpleStorageContract[_simpleStorageIndex].setFavoriteNumber(_simpleStorageNumber);
     }
     
-    function getStorageFactoryNumber(uint256 _simpleStorageIndex) public view returns (uint256) {
+    function getStorageFactoryNumber(uint256 _simpleStorageIndex) external view returns (uint256) {
         return getSimpleStorageContract[_simpleStorageIndex].getFavoriteNumber();
     }
 }
